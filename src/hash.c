38fe4150b30520b36abc4567dd3a762a1e4b4a2a
Erik Zscheile <erik.zscheile.ytrizja@gmail.com>
2018-08-14 19:13:42 +0200

    fix target names; +feature:cmake_export_targets

--- src/hash.c
+++ src/hash.c
@@ -1,10 +1,10 @@
-/** zsparsell hash.c
+/** llzs hash.c
     (C) 2018 Erik Zscheile
     License: MIT
  **/
 #include <stdint.h>
 
-void zsparsell_hash_combine(uintmax_t *seed, const uintmax_t o) {
+void llzs_hash_combine(uintmax_t *seed, const uintmax_t o) {
   // source of rndst : https://stackoverflow.com/questions/5889238/why-is-xor-the-default-way-to-combine-hashes#comment54810251_27952689
   // rndst := inverse of golden ratio as a ... fixed point fraction
   // the following uses more entropy on 64bit (e.g. when available)
