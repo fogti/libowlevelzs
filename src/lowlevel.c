e83bc2aadaa9e7664e2828868fa0d493b6909caf
Erik Zscheile <erik.zscheile.ytrizja@gmail.com>
2018-12-22 23:42:03 +0100

    +my_signal_norestart

--- src/lowlevel.c
+++ src/lowlevel.c
@@ -7,6 +7,7 @@
 
 #include "zsig.h"
 #include <signal.h>
+zs_attrib(cold)
 void my_signal(const int sig_nr, const sighandler_t signalhandler) {
 #ifdef WIN32
 /* win32 -- source: https://stackoverflow.com/questions/32389905/sigaction-and-porting-linux-code-to-windows */
@@ -22,3 +23,15 @@ void my_signal(const int sig_nr, const sighandler_t signalhandler) {
   sigaction(sig_nr, &sig, 0);
 #endif
 }
+
+#ifndef WIN32
+/* this function is currently unsupported on platforms without sigaction */
+zs_attrib(cold)
+void my_signal_norestart(const int sig_nr, const sighandler_t signalhandler) {
+  struct sigaction sig;
+  sig.sa_handler = signalhandler;
+  sig.sa_flags   = 0;
+  sigemptyset(&sig.sa_mask);
+  sigaction(sig_nr, &sig, 0);
+}
+#endif
--- src/zsig.h
+++ src/zsig.h
@@ -3,11 +3,16 @@
     License: MIT
  **/
 #pragma once
+#include "cxa_noexcept.h"
+#include "llzs_config.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
   typedef void (*sighandler_t)(int);
-  void my_signal(int sig_nr, sighandler_t signalhandler);
+  void my_signal(int sig_nr, sighandler_t signalhandler) noexcept;
+#ifndef WIN32
+  void my_signal_norestart(const int sig_nr, const sighandler_t signalhandler) noexcept;
+#endif
 #ifdef __cplusplus
 }
 #endif
