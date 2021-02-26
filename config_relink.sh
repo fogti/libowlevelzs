ac85aa781ebcb8a577c5bca8a0fce61e08d41114
zseri <zseri.devel@ytrizja.de>
2021-02-26 20:21:21 +0100

    get rid of config_relink.sh

--- config_relink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-[ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] && exit 1
-CMAKE="$1" GENDIR="$2" FILEN="$3"
-mkdir -p "$GENDIR/zs/ll"
-cd "$GENDIR/zs/ll" || exit 1
-rm -f "$3"
-exec "$CMAKE" -E create_symlink "../../$3" "$3"
